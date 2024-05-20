d9bd2bd333fb7a800354a9bbc87f57667e65b895
SSerregg <shaytar1992@gmail.com>
2024-05-20 18:44:55 +0800

    ver 0.1

---
+++ index.php
@@ -0,0 +1,170 @@
+<?php 
+$example_persons_array = [
+    [
+        'fullname' => 'Иванов Иван Иванович',
+        'job' => 'tester',
+    ],
+    [
+        'fullname' => 'Степанова Наталья Степановна',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Пащенко Владимир Александрович',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Громов Александр Иванович',
+        'job' => 'fullstack-developer',
+    ],
+    [
+        'fullname' => 'Славин Семён Сергеевич',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Цой Владимир Антонович',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Быстрая Юлия Сергеевна',
+        'job' => 'PR-manager',
+    ],
+    [
+        'fullname' => 'Шматко Антонина Сергеевна',
+        'job' => 'HR-manager',
+    ],
+    [
+        'fullname' => 'аль-Хорезми Мухаммад ибн-Муса',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Бардо Жаклин Фёдоровна',
+        'job' => 'android-developer',
+    ],
+    [
+        'fullname' => 'Шварцнегер Арнольд Густавович',
+        'job' => 'babysitter',
+    ],
+];
+
+function getPartsFromFullname ($fullName) {
+    $workpiece = ['surname','name','patronomyc'];  
+    $fromFullName = explode(' ', $fullName);
+
+return array_combine($workpiece, $fromFullName);
+}
+
+print_r(getPartsFromFullname ($example_persons_array[0]['fullname']));
+echo '<br>';
+
+function getFullnameFromParts($surname = 'Иванов', $name = 'Иван', $patronomyc = 'Иванович') {
+return $surname.' '.$name.' '.$patronomyc;
+}
+
+print_r(getFullnameFromParts());
+echo '<br>';
+
+function getShortName($fullName) {
+    $division = getPartsFromFullname ($fullName);
+    return $division['name'].' '.mb_substr($division['surname'], 0, 1).'.';
+}
+
+print_r(getShortName($example_persons_array[0]['fullname']));
+echo '<br>';
+
+function getGenderFromName($fullName = 'Иванов Иван Иванович') {
+    $partsName = getPartsFromFullname($fullName);
+    $count = 0;
+    if (mb_substr($partsName['surname'], -1) == 'в'){
+        $count++;
+    } elseif (mb_substr($partsName['surname'], -2) == 'ва'){
+        $count--;  
+    }
+    if (mb_substr($partsName['name'], -1) == 'н'){
+        $count++;
+    } elseif (mb_substr($partsName['name'], -1) == 'а'){
+        $count--;  
+    }
+    if (mb_substr($partsName['patronomyc'], -2) == 'ич'){
+        $count++;
+    } elseif (mb_substr($partsName['patronomyc'], -3) == 'вна'){
+        $count--;   
+    }
+    if ($count > 0) {
+        return 1;
+    } elseif ($count < 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+print_r(getGenderFromName());
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+function getGenderDescription ($namesArray) {
+
+$maleCount = count(array_filter($namesArray, function($array){
+    $simile = getGenderFromName($array['fullname']);
+    if ($simile == 1){
+        return $array['fullname'];
+    }
+}));
+
+$femaleCount = count(array_filter($namesArray, function($array){
+    $simile = getGenderFromName($array['fullname']);
+    if ($simile == -1){
+        return $array['fullname'];
+    }
+}));
+
+$xxxCount = count(array_filter($namesArray, function($array){
+    $simile = getGenderFromName($array['fullname']);
+    if ($simile == 0){
+        return $array['fullname'];
+    }
+}));
+
+$fullArrayCount = count($namesArray);
+
+echo 'Гендарный состав аудитории:';
+echo '<br>';
+echo '-------------------------------------';
+echo '<br>';
+echo 'Мужчины - ';
+print_r(round($maleCount/$fullArrayCount*100, 1));
+echo '%<br>';
+echo 'Женщины - ';
+print_r(round($femaleCount/$fullArrayCount*100, 1));
+echo '%<br>';
+echo 'Не удалось определить - ';
+print_r(round($xxxCount/$fullArrayCount*100, 1));
+echo '%<br>';
+}
+
+getGenderDescription($example_persons_array);
+echo '<br>';
+echo '<br>';
+
+function getPerfectPartner($surname, $name, $patronomyc, $originalArray) {
+    $surname = mb_convert_case($surname, MB_CASE_TITLE_SIMPLE);
+    $name = mb_convert_case($name, MB_CASE_TITLE_SIMPLE);
+    $patronomyc = mb_convert_case($patronomyc, MB_CASE_TITLE_SIMPLE);
+    $fullName = getFullnameFromParts($surname, $name, $patronomyc);
+    echo getShortName($fullName).' + ';
+    $gender = getGenderFromName($fullName);
+    function opposite ($array, $gender) {
+    $chosen = $array[array_rand($array, 1)]['fullname'];
+    $genderChosen = getGenderFromName($chosen);
+    if ($gender == $genderChosen || $genderChosen==0){
+        opposite ($array, $gender);
+    }else{ echo getShortName($chosen).' =<br>';
+    } 
+    }
+    opposite ($originalArray, $gender);
+    print json_decode('"\u2600"');
+    echo ' Идеально на '.rand(50, 100).'% ';
+    print json_decode('"\u2600"');
+}
+getPerfectPartner('Хохлов','Юра','Иванович',$example_persons_array);   
+?>
